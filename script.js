1e4e4e75349ca910f02d14f92ef87cc830445a31
Nicolas <nickmnunes@hotmail.com>
2022-09-10 02:32:10 -0300

    adicionado carrossel

--- script.js
+++ script.js
@@ -42,3 +42,34 @@ window.addEventListener("scroll", function () {
     backTopBtn.classList.remove("active");
   }
 });
+
+
+//carrossel
+
+  let time = 2500;
+  let currentImageIndex = 0;
+  let images = document.querySelectorAll("#slider img");
+  let max = images.length;
+
+
+function nextImage() {
+
+  images[currentImageIndex].classList.remove("selected");
+
+  currentImageIndex++;
+
+  if(currentImageIndex >= max){
+    currentImageIndex = 0;
+  }
+
+  images[currentImageIndex].classList.add("selected");
+}
+
+
+function start(){
+  setInterval(() => {
+    nextImage();
+  }, time)
+}
+
+window.addEventListener("load", start)
